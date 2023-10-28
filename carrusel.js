6cf9b1c63c34b6084750bbc2f2df93858a994e16
brunoruarte <brunoruarte@gmail.com>
2023-10-28 18:54:07 -0300

    Carruserl 2
    Estructura funcionando

--- carrusel.js
+++ carrusel.js
@@ -1,37 +1,37 @@
-var arreglo_img = ['Recursos/img_carrusel/1.png', 'Recursos/img_carrusel/2.png', 'Recursos/img_carrusel/3.png'];
+let arreglo_img = ['../Recursos/img_carrusel/1.png', '../Recursos/img_carrusel/2.png', '../Recursos/img_carrusel/3.png'];
 let contador = 0;
 
 function carrusel(contenedor) {
-    contenedor.addEventListener('click', e => {
-        let atras = contenedor.querySelector('.botonatras');
-        let adelante = contenedor.querySelector('.botonadelante');
-        let img = contenedor.querySelector('ímg');
-        let seleccionado = e.target;
+    contenedor.addEventListener('click', elemento => {
+        let atras = contenedor.querySelector('.atras');
+        let adelante = contenedor.querySelector('.adelante');
+        let imagen = contenedor.querySelector('.imagen_carru');
+        let seleccionado = elemento.target;
 
         if (seleccionado == atras) {
             if (contador > 0) {
-                img.src = arreglo_img[contador-1];
+                imagen.src = arreglo_img[contador-1];
                 contador -= 1;
             } else {
-                img.src = arreglo_img[arreglo_img.length-1];
+                imagen.src = arreglo_img[arreglo_img.length-1];
                 contador = arreglo_img.length-1;
             }
         }
 
         if (seleccionado == adelante) {
             if (contador < arreglo_img.length-1) {
-                img.src = arreglo_img[contador+1];
+                imagen.src = arreglo_img[contador+1];
                 contador += 1;
             } else {
-                img.src = arreglo_img[0];
+                imagen.src = arreglo_img[0];
                 contador = 0;
             }
         }
     });
 }
 
-document.addEventListener("DOMContentLoaded"); () => {
-    let contenedor = document.querySelector('.contenedor');
+document.addEventListener("DOMContentLoaded", () => {
+    let contenedor = document.querySelector('.omaigad');
 
     carrusel(contenedor);
-}
+});
